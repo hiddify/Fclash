e7805ddf41ec357dd68031e897264c8f8c3f7351
problematicconsumer <problematicconsumer@protonmail.com>
2023-05-11 17:44:27 +0330

    added settings feature

---
+++ lib/features/settings/view/settings_page.dart
@@ -0,0 +1,78 @@
+import 'package:fclash/core/clash/clash.dart';
+import 'package:fclash/core/core_providers.dart';
+import 'package:fclash/core/prefs/theme/theme.dart';
+import 'package:fclash/features/settings/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:recase/recase.dart';
+
+class SettingsPage extends HookConsumerWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final t = ref.watch(Core.translations);
+
+    final theme = ref.watch(ThemeController.provider);
+    final themeController = ref.watch(ThemeController.provider.notifier);
+    final clashOverrides = ref.watch(ClashController.provider);
+    final clashController = ref.watch(ClashController.provider.notifier);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(t.general.settings.titleCase),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Text(t.settings.appearance.titleCase),
+          ),
+          ListTile(
+            title: Text(t.settings.themeMode.titleCase),
+            trailing: ThemeModeSwitch(
+              themeMode: theme.themeMode,
+              onChanged: (value) {
+                themeController.change(themeMode: value);
+              },
+            ),
+            onTap: () {
+              if (Theme.of(context).brightness == Brightness.light) {
+                themeController.change(themeMode: ThemeMode.dark);
+              } else {
+                themeController.change(themeMode: ThemeMode.light);
+              }
+            },
+          ),
+          SwitchListTile(
+            title: Text(t.settings.darkIsBlack.titleCase),
+            value: theme.darkIsBlack,
+            onChanged: (value) {
+              themeController.change(darkIsBlack: value);
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Text(t.settings.proxy.titleCase),
+          ),
+          ListTile(
+            title: Text(t.settings.httpPort),
+            trailing: Text(clashOverrides.overrides.httpPort.toString()),
+            onTap: () {
+              SettingsInputDialog<int>(
+                title: t.settings.httpPort,
+                initialValue: clashOverrides.overrides.httpPort,
+                onConfirm: (value) async {
+                  final input = int.tryParse(value);
+                  if (input == null) return;
+                  await clashController.changeOverrides(httpPort: some(input));
+                },
+              ).show(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/settings/widgets/settings_input_dialog.dart
@@ -0,0 +1,63 @@
+import 'package:fclash/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SettingsInputDialog<T> extends HookConsumerWidget with PresLogger {
+  const SettingsInputDialog({
+    super.key,
+    required this.title,
+    required this.onConfirm,
+    this.initialValue,
+    this.icon,
+  });
+
+  final String title;
+  final ValueChanged<String> onConfirm;
+  final T? initialValue;
+  final IconData? icon;
+
+  Future<void> show(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) => this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final localizations = MaterialLocalizations.of(context);
+
+    final textController = useTextEditingController(
+      text: initialValue?.toString(),
+    );
+
+    return AlertDialog(
+      title: Text(title),
+      icon: Icon(icon),
+      content: TextFormField(
+        controller: textController,
+        inputFormatters: [
+          FilteringTextInputFormatter.singleLineFormatter,
+        ],
+        autovalidateMode: AutovalidateMode.always,
+      ),
+      actions: [
+        TextButton(
+          onPressed: () async {
+            await Navigator.of(context).maybePop();
+          },
+          child: Text(localizations.cancelButtonLabel.toUpperCase()),
+        ),
+        TextButton(
+          onPressed: () async {
+            onConfirm(textController.value.text);
+            await Navigator.of(context).maybePop();
+          },
+          child: Text(localizations.okButtonLabel.toUpperCase()),
+        ),
+      ],
+    );
+  }
+}
